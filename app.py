a6ff6da1221cf87326d851d759a339fe0aeeb021
Iranpour-Aryan <77175291+Iranpour-Aryan@users.noreply.github.com>
2021-12-21 13:58:36 -0500

    Udemy Course project for Web Development
    A small blog using HTML, CSS for front-end. Flask, MongoDB, Python for back-end. Application is deployed from Heroku. Good practice before creating my Portfolio and Crypto Market Project.

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template, request
+import datetime
+from pymongo import MongoClient
+import certifi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_app():
+    ca = certifi.where()
+    app = Flask(__name__)
+    client = MongoClient(os.environ.get("MONGODB_URI"), tlsCAFile=ca)
+    app.db = client.microblog
+
+
+    @app.route("/", methods=["GET","POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content").strip()
+            formatted_date = datetime.datetime.today().strftime("%y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+
+        entries_with_date = [
+            (entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"],"%y-%m-%d").strftime("%b %d")
+            )
+
+            for entry in app.db.entries.find({})
+
+        ]
+
+        return render_template("home.html", entries=entries_with_date)
+
+    return app
